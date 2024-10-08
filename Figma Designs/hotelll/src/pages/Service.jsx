4c871e7d27c6ebd29ec61c5c42983d9df6f6cec6
Clentan <clentanchauke9089@gmail.com>
2024-10-08 11:08:46 +0200

    .

--- Figma Designs/hotelll/src/pages/Service.jsx
+++ Figma Designs/hotelll/src/pages/Service.jsx
@@ -1,9 +1,13 @@
+import { Button } from "@nextui-org/react"
 
 
 const Service = () => {
   return (
     <div>
         <h1>Service</h1>
+        <Button onClick={() => {
+          window.location.href = 'D:/Hotel-mlabFigma Designs/hotelll/src/component/dashboard/material-dashboard-master/index.html'
+        }}>ooijf</Button>
       
     </div>
   )
